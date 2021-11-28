7149bb602fb13309eb7a282c09034cf4b8111ea9
Ignacio <nachonar2020@gmail.com>
2021-11-28 17:33:15 -0300

    commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => {
+	
+	return (
+			 
+			<h1>
+				Title
+			</h1>
+			
+	);
+	
+	
+};
+
+//					QUÉ RENDERIZAR 		DÓNDE RENDERIZAR
+render(React.createElement(App), document.getElementById("root"));
